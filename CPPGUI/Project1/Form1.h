5c32107832338becd6db7f43d9c8e9296eb422ee
ninadthite <neenadthite@outlook.com>
2024-05-20 22:26:32 +0530

    added buttons in GUI

---
+++ CPPGUI/Project1/Form1.h
@@ -0,0 +1,108 @@
+#pragma once
+
+namespace CppCLRWinformsProjekt {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Zusammenfassung für Form1
+	/// </summary>
+	public ref class Form1 : public System::Windows::Forms::Form
+	{
+	public:
+		Form1(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Konstruktorcode hier hinzufügen.
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		~Form1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	protected:
+
+	private:
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->ForeColor = System::Drawing::SystemColors::Highlight;
+			this->button1->Location = System::Drawing::Point(35, 0);
+			this->button1->Margin = System::Windows::Forms::Padding(2);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(57, 52);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"click me";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
+			this->button1->MouseEnter += gcnew System::EventHandler(this, &Form1::button1_MouseEnter);
+			this->button1->MouseLeave += gcnew System::EventHandler(this, &Form1::button1_MouseLeave);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(55, 89);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"button2";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(185, 159);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Margin = System::Windows::Forms::Padding(2);
+			this->Name = L"Form1";
+			this->Text = L"Form1";
+			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void Form1_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		MessageBox::Show("This is my first CV++ Project");
+	}
+	private: System::Void button1_MouseEnter(System::Object^ sender, System::EventArgs^ e) {
+		button1->Text = "Do not click me";
+	}
+	private: System::Void button1_MouseLeave(System::Object^ sender, System::EventArgs^ e) {
+		button1->Text = "Click me";
+	}
+	};
+}
